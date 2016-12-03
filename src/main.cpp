d5cc895bb51f47cab27feb4acc070b6d28990794
Kai Fung <fung1950@ad.wlu.ca>
2016-12-03 18:26:45 -0500

    Added Bow.hpp and Bow.cpp, displays bow

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "Matrix.hpp"
 #include "Light.hpp"
 #include "Landscape/Landscape.hpp"
+#include "Bow.hpp"
 
 // declaring the size of the window
 GLint winWidth = 800, winHeight = 800;
@@ -23,13 +24,15 @@ GLfloat red = 1.0, green = 1.0, blue = 1.0;  //color
 Camera myCamera;
 Landscape myLandscape;
 Light myLight;
+Bow myBow;
 
 void init(void) {
 	myLight.translate(1.5,1.5,1.5);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	myCamera.setProjectionMatrix();
-}
 
+
+}
 
 void reset(void) {
 	glFlush();
@@ -44,6 +47,9 @@ void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	myCamera.setProjectionMatrix();
 	myLandscape.draw_landscape();
+
+	myBow.draw();
+
 	glFlush();
 	glutSwapBuffers();
 }
@@ -74,6 +80,21 @@ void keyPressed (unsigned char key, int x, int y) {
 	}
 }
 
+void reshape(int w, int h)
+{
+   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   if (w <= h)
+      glOrtho(-5.0, 5.0, -5.0*(GLfloat)h/(GLfloat)w,
+               5.0*(GLfloat)h/(GLfloat)w, -5.0, 5.0);
+   else
+      glOrtho(-5.0*(GLfloat)w/(GLfloat)h,
+               5.0*(GLfloat)w/(GLfloat)h, -5.0, 5.0, -5.0, 5.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
+
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -82,7 +103,9 @@ int main(int argc, char** argv) {
 	glutCreateWindow("Archery Simulation by Jason and Joe");
 	init();
 	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyPressed); // movement key function
 	glutMainLoop();
+
 	return 0;
 }
